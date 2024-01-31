f89cfd8d1d15bc62e81e722c407fc47a54c75c72
Madhumitha <madhumithavenu03@gmail.com>
2024-01-31 11:53:45 +0530

    Routes config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import BookingCar from './pages/BookingCar/BookingCar';
 
 function App() {
   return (
     <div className="App">
-      Hello
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/bookingcar' element={<BookingCar/>}/>
+     </Routes>
     </div>
   );
 }
