86813ddeb6bd24ff50bdf1d0386f8b0a3983853e
Madhumitha <madhumithavenu03@gmail.com>
2024-01-31 12:11:36 +0530

    Adding navbar

---
+++ src/components/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,21 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+function DefaultLayout() {
+    return (
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Cars
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
+    )
+}
+
+export default DefaultLayout;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import DefaultLayout from '../../components/DefaultLayout/DefaultLayout';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+            <DefaultLayout />
+            <h1>Home</h1>
+        </div>
+    )
 }
 
 export default Home;
