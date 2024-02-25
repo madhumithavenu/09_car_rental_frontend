8eec9d8e4f5ef883f497c55db25939370e505204
Madhumitha <madhumithavenu03@gmail.com>
2024-02-25 10:34:42 +0530

    designing home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,17 @@ import React, { useEffect } from 'react'
 import Header from '../../components/Header/Header.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCars } from '../../store/car_actions.js';
+import { Box } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 function Home() {
     const cars = useSelector((state) => state.car.cars);
@@ -13,9 +24,20 @@ function Home() {
 
     return (
         <div>
-            <Header />
-            <h1>Home</h1>
-            <h1>Length of the car {cars.length}</h1>
+            <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+                <Box gridColumn="span 8">
+                    <Item></Item>
+                </Box>
+                <Box gridColumn="span 4">
+                    <Item>xs=4</Item>
+                </Box>
+                <Box gridColumn="span 4">
+                    <Item>xs=4</Item>
+                </Box>
+                <Box gridColumn="span 8">
+                    <Item>xs=8</Item>
+                </Box>
+            </Box>
         </div>
     )
 }
