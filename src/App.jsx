78195679f0f82714885736281b28301fb7ae6f0f
Madhumitha <madhumithavenu03@gmail.com>
2024-03-08 15:23:00 +0530

    Designing login and Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
-import Login from './pages/Login/Login';
+import Auth from './pages/Auth/Auth';
 import BookingCar from './pages/BookingCar/BookingCar';
 import Navbar from './pages/Navbar/Navbar';
 
@@ -13,7 +13,7 @@ function App() {
       <Navbar/>
      <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
+      <Route path='/auth' element={<Auth/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/bookingcar' element={<BookingCar/>}/>
      </Routes>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import carSlice from "./car_slice";
 import alertSlice from "./alert_slice";
+import authSlice from "./auth_slice";
 
 const store = configureStore({
     reducer: {
+        auth: authSlice.reducer,
         car: carSlice.reducer,
         alert: alertSlice.reducer
     }
